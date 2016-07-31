90976c5c06889eb21d463239fdefe2dcf7874984
AustinGCState <austinmkpc@hotmail.com>
2016-07-31 19:29:04 -0400

    Added Pokemon Model

---
+++ Comp2007-Assignment2/Models/Pokemon.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Comp2007_Assignment2.Models
+{
+    public class Pokemon
+    {
+
+        public int PokemonId { get; set; }
+        public string Name { get; set; }
+    }
+}
