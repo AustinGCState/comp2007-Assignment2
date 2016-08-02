6f6e2806a15339cc3ff69aa2980f532d8e76eb05
AustinGCState <austinmkpc@hotmail.com>
2016-08-01 23:26:48 -0400

    Added StoreManagerController

---
+++ Comp2007-Assignment2/Controllers/StoreManagerController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Comp2007_Assignment2.Models;
+
+namespace Comp2007_Assignment2.Controllers
+{
+    public class StoreManagerController : Controller
+    {
+        private PokemonBytesContext db = new PokemonBytesContext();
+
+        // GET: StoreManager
+        public async Task<ActionResult> Index()
+        {
+            var dishes = db.Dishes.Include(d => d.Type);
+            return View(await dishes.ToListAsync());
+        }
+
+        // GET: StoreManager/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Dish dish = await db.Dishes.FindAsync(id);
+            if (dish == null)
+            {
+                return HttpNotFound();
+            }
+            return View(dish);
+        }
+
+        // GET: StoreManager/Create
+        public ActionResult Create()
+        {
+            ViewBag.TypeId = new SelectList(db.Types, "TypeID", "Name");
+            return View();
+        }
+
+        // POST: StoreManager/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "DishID,PokemonId,TypeId,Name,Price,DishImageUrl")] Dish dish)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Dishes.Add(dish);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.TypeId = new SelectList(db.Types, "TypeID", "Name", dish.TypeId);
+            return View(dish);
+        }
+
+        // GET: StoreManager/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Dish dish = await db.Dishes.FindAsync(id);
+            if (dish == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.TypeId = new SelectList(db.Types, "TypeID", "Name", dish.TypeId);
+            return View(dish);
+        }
+
+        // POST: StoreManager/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "DishID,PokemonId,TypeId,Name,Price,DishImageUrl")] Dish dish)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(dish).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.TypeId = new SelectList(db.Types, "TypeID", "Name", dish.TypeId);
+            return View(dish);
+        }
+
+        // GET: StoreManager/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Dish dish = await db.Dishes.FindAsync(id);
+            if (dish == null)
+            {
+                return HttpNotFound();
+            }
+            return View(dish);
+        }
+
+        // POST: StoreManager/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Dish dish = await db.Dishes.FindAsync(id);
+            db.Dishes.Remove(dish);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
